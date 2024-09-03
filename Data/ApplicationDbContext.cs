9ac8768d609368172cf278353d18424402a7c762
Mosquera31 <leison3131@hotmail.com>
2024-09-03 08:49:56 -0500

    feat:the Database has been connected

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace VetCare_BackEnd.Data;
+
+public class ApplicationDbContext:DbContext
+{
+    public DbSet<User> Users { get; set; }
+
+    public DbSet<DocumentType> DocumentTypes { get; set; }
+
+    public DbSet<Role> Roles { get; set; }
+
+    public DbSet<Appointment> Appointments { get; set; }
+
+    public DbSet<Pet> Pets { get; set; }
+
+    public DbSet<AppointmentType> AppointmentTypes { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+}
