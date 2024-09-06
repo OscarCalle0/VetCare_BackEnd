b47097cf6eeb73ebabbc8cfd5f7776a26bec5a18
Z4pata <zapata.devs@gmail.com>
2024-09-06 08:39:09 -0500

    Feat: the Data Transfer Object of Pet has been create

---
+++ Models/PetDTO.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VetCare_BackEnd.Models
+{
+    public class PetDTO
+    {
+    public required string Name { get; set; }
+    public required string Breed { get; set; }
+    public required string Weight { get; set; }
+    public required DateOnly BirthDate { get; set; }
+    public required string Sex { get; set; }
+    public required int user_id { get; set; }
+    }
+}
