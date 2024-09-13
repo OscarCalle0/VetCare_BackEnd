16458121deb6d1db738de39864b309f701ad72e6
Jhon-Develop <asprillajhon73@gmail.com>
2024-09-13 10:21:19 -0500

    ready

--- Program.cs
+++ Program.cs
@@ -55,11 +55,9 @@ var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 // //para revisar so las solictudes estan llegando
 // app.Use(async (context, next) =>
