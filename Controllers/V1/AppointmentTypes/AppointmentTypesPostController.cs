ad6ec3b1a3e62277ef662eccf03e963b54082c55
Jzulu22x <juanzr1015@gmail.com>
2024-09-20 09:35:52 -0500

    feat: creation of comments for the appointment type controller

--- Controllers/V1/AppointmentTypes/AppointmentTypesPostController.cs
+++ Controllers/V1/AppointmentTypes/AppointmentTypesPostController.cs
@@ -8,6 +8,15 @@ using VetCare_BackEnd.Models;
 namespace VetCare_BackEnd.Controllers.V1.AppointmentTypes;
 public partial class AppointmentTypesController
 {
+    
+    /// <summary>
+    /// Create a new appointment type
+    /// </summary>
+    /// <param name="newAppointmentType">The appointment type object to be created</param>
+    /// <returns>A response indicating the result of the appointment type creation.</returns>
+    /// <response code="200">Returns a message indicating the appointment type was successfully created.</response>
+    /// <response code="400">If the input data is invalid.</response>
+    /// <response code="500">If there is a server error.</response>
     [HttpPost("create")]
     public async Task<IActionResult> Create(AppointmentType newAppointmentType)
     {
